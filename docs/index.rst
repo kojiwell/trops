cade1cfb65aedd4c01550ec3fb0b63bd7e7ec7a5
Koji Tanaka <kojiwelly@gmail.com>
2022-03-27 20:20:55 +0900

    Change from README.md to README.rst (#29)

--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,2 @@
-.. Trops documentation master file, created by
-   sphinx-quickstart on Thu Mar 24 23:04:55 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. include:: ../README.rst
 
-Welcome to Trops's documentation!
-=================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ setuptools.setup(
     author="Koji Tanaka",
     author_email="kojiwelly@gmail.com",
     description=("Track operations"),
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     license="MIT",
     keywords="linux system administration",
     url="http://github.com/kojiwell/trops",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    long_description=read('README.md'),
+    long_description=read('README.rst'),
     python_requires='>=3.8',
     install_requires=[
         "tabulate >= 0.8.9"
