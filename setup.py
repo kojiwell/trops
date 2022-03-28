ae6f1377a524e8480726cecb501f6dbfff47613f
Koji Tanaka <kojiwelly@gmail.com>
2022-03-28 23:45:54 +0900

    bump to 0.1.19

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="trops",
-    version="0.1.18",
+    version="0.1.19",
     author="Koji Tanaka",
     author_email="kojiwelly@gmail.com",
     description=("Track operations"),
