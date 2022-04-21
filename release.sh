5194fc66cd61f2489f7e68cbcb31101611ef0fd1
Koji Tanaka <kojiwelly@gmail.com>
2022-04-21 10:53:05 +0900

    Release 0.2.7 (#65)

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# release.sh - update files for new release
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./release.sh <version>"
+fi
+
+sed -i "s/release =.*/release = '$1'/g" docs/conf.py
+sed -i "s/    version=.*/    version=\"$1\",/g" setup.py
+sed -i "s/__version__ =.*/__version__ = '$1'/g" src/trops/release.py
