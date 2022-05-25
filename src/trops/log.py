f6bd851155a2689363da8fceb43450de03476df4
Koji Tanaka <kojiwelly@gmail.com>
2022-05-25 14:03:39 +0900

    trops log: Print all lines with the tags of the current session (#80)

--- src/trops/log.py
+++ src/trops/log.py
@@ -25,28 +25,30 @@ class TropsLog(Trops):
         """Print trops log"""
 
         log_file = self.trops_logfile
-        numlines = 15
 
-        # -t/--tail <num>
-        if self.args.tail:
-            numlines = self.args.tail
-
-        # -a/--all
-        if self.args.all:
-            with open(log_file) as ff:
-                for line in ff.readlines():
+        with open(log_file) as ff:
+            if self.args.tail:
+                lines = ff.readlines()[-self.args.tail:]
+            else:
+                lines = ff.readlines()
+            for line in lines:
+                if self.args.all or not self.trops_tags:
                     print(line, end='')
-        else:
-            with open(log_file) as ff:
-                for line in ff.readlines()[-numlines:]:
+                elif f'TROPS_TAGS={self.trops_tags}' in line:
                     print(line, end='')
+                else:
+                    pass
 
         if self.args.follow:
             ff = open(log_file, "r")
             try:
                 lines = self._follow(ff)
                 for line in lines:
-                    print(line, end='')
+                    if self.args.all:
+                        print(line, end='')
+                    elif f'TROPS_TAGS={self.trops_tags}' in line:
+                        print(line, end='')
+
             except KeyboardInterrupt:
                 print('\nClosing trops log...')
 
