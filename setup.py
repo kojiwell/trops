91212a71d37e0f7a4a83f219d93310024e139756
Koji Tanaka <kojiwelly@gmail.com>
2022-06-14 01:07:12 +0900

    Release 0.2.13

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="trops",
-    version="0.2.12",
+    version="0.2.13",
     author="Koji Tanaka",
     author_email="kojiwelly@gmail.com",
     description=("Track operations"),
