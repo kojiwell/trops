756b1af7c15a1a261c40db49cb076376d763dc04
Koji Tanaka <kojiwelly@gmail.com>
2022-06-28 10:07:36 +0900

    Add the pypi uploading process in the release.sh (#94)

--- release.sh
+++ release.sh
@@ -9,3 +9,7 @@ fi
 sed -i "s/release =.*/release = '$1'/g" docs/conf.py
 sed -i "s/    version=.*/    version=\"$1\",/g" setup.py
 sed -i "s/__version__ =.*/__version__ = '$1'/g" src/trops/release.py
+
+python3 setup.py sdist
+python3 setup.py bdist_wheel
+twine upload --repository pypi dist/trops-${1}.tar.gz
