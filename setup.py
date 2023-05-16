793ae2c7fe86cab50c8f23b7702e26036590a596
Koji Tanaka <kj.tanaka@gmail.com>
2023-05-16 11:58:12 +0900

    Fix the pip error (#106)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="trops",
-    version="0.2.17",
+    version="0.2.18",
     author="Koji Tanaka",
     author_email="kojiwelly@gmail.com",
     description=("Track operations"),
