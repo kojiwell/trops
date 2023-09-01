685053e69a5c28b1f5c7ea80c000c99aa16d6425
Koji Tanaka <kj.tanaka@gmail.com>
2023-09-01 10:23:29 +0900

    Add test_trops (#107)

--- tests/test_tee.py
+++ tests/test_tee.py
@@ -1,5 +1,4 @@
 import io
-import os
 import argparse
 import pytest
 
@@ -18,21 +17,6 @@ def setup_tee_args():
         add_tee_subparsers(subparsers)
         args, other_args = parser.parse_known_args()
     return args, other_args
-
-
-def test_trops_dir(monkeypatch, setup_tee_args):
-    args, other_args = setup_tee_args
-
-    # Test ~ (tilda) in TROPS_DIR
-    monkeypatch.setenv("TROPS_DIR", '~/trops')
-    tt1 = TropsTee(args, other_args)
-    assert tt1.trops_dir == os.path.expanduser('~/trops')
-    assert tt1.file_path == 'path/to/file'
-
-    # Test an environment variable (e.g. HOME) in TROPS_DIR
-    monkeypatch.setenv("TROPS_DIR", '$HOME/trops')
-    tt2 = TropsTee(args, other_args)
-    assert tt2.trops_dir == os.path.expanduser('~/trops')
 
 
 def test_sys_stdin_read(monkeypatch, setup_tee_args):
