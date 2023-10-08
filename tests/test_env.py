89cd118cb92f85efb818d62a6d570deb76de1c93
Koji Tanaka <kojiwelly@gmail.com>
2023-10-09 07:20:03 +0900

    Create set_env_args func in test_env

--- tests/test_env.py
+++ tests/test_env.py
@@ -8,7 +8,12 @@ from trops.env import TropsEnv, add_env_subparsers
 
 
 @pytest.fixture
-def setup_env_args():
+def setup_env_args(monkeypatch, tmp_path):
+  
+    trops_dir = tmp_path + '/trops'
+    monkeypatch.setenv("TROPS_DIR", trops_dir)
+    monkeypatch.setenv("TROPS_TAGS", '#123,TEST')
+
     with patch("sys.argv", ["trops", "env", "create", "testenv"]):
         parser = argparse.ArgumentParser(
             prog='trops', description='Trops - Tracking Operations')
@@ -21,6 +26,4 @@ def setup_env_args():
 def test_env(monkeypatch, setup_env_args):
     args, other_args = setup_env_args
 
-    monkeypatch.setenv("TROPS_DIR", '/tmp/trops')
-    monkeypatch.setenv("TROPS_TAGS", '#123,TEST')
     te = TropsEnv(args, other_args)
