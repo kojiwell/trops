b3121e6b7083b9a7aa3664bb2141584b6cf529b0
Koji Tanaka <kojiwelly@gmail.com>
2023-12-05 06:08:17 +0000

    Release version 0.2.21

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Koji Tanaka'
 author = 'Koji Tanaka'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.21'
+release = '0.2.22'
 
 
 # -- General configuration ---------------------------------------------------
