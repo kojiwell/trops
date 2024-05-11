96899c72a531a31e20149a294fd60bad60f034ba
Koji Tanaka <kojiwelly@gmail.com>
2024-05-12 08:35:58 +0900

    Update test_capcmd.py

--- tests/test_capcmd.py
+++ tests/test_capcmd.py
@@ -24,6 +24,7 @@ def setup_capcmd_args(monkeypatch):
 def test_capcmd(monkeypatch, setup_capcmd_args):
     args, other_args = setup_capcmd_args
 
+    monkeypatch.setenv("TROPS_TAGS", '#123,TEST')
+
     tcc = TropsCapCmd(args, other_args)
-    #assert tcc.trops_dir == '/tmp/trops'
-    assert tcc.trops_header[3] == ''
+    assert tcc.trops_header[3] == '#123,TEST'
