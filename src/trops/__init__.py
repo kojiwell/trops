457976d2d0d397d4968cd8195f9c9899aa323c69
Koji Tanaka <kojiwelly@gmail.com>
2024-07-03 09:24:05 +0900

    Update __init__.py and init.py

--- src/trops/__init__.py
+++ src/trops/__init__.py
@@ -1,17 +1,27 @@
 import os
 import sys
 
-if sys.version_info < (3, 8):
+from .utils import yes_or_no
+
+# Check Python version
+required_python_version = (3, 8)
+if sys.version_info < required_python_version:
     raise SystemExit(
-        "ERROR: Ansible requires Python 3.8 or newer on the controller. "
-        f"Current version: {''.join(sys.version.splitlines())}"
+        f"ERROR: This program requires Python {required_python_version[0]}.{required_python_version[1]} or newer. "
+        f"Current version: {'.'.join(map(str, sys.version_info[:3]))}"
     )
 
-if not os.getenv('TROPS_DIR'):
-    raise SystemExit('ERROR: TROPS_DIR has not been set')
+# Ensure TROPS_DIR environment variable is set
+trops_dir = os.getenv('TROPS_DIR')
+if not trops_dir:
+    raise SystemExit('ERROR: The TROPS_DIR environment variable has not been set.')
+
+# Resolve any environment variables and user symbols in TROPS_DIR path
+trops_dir_expanded = os.path.expanduser(os.path.expandvars(trops_dir))
 
 # Check if TROPS_DIR is an existing directory
-if not os.path.isdir(os.path.expanduser(os.path.expandvars(os.getenv('TROPS_DIR')))):
-    raise SystemExit(
-        "ERROR: TROPS_DIR is not an existing directory. "
-        f"Current TROPS_DIR: {os.getenv('TROPS_DIR')}")
+if not os.path.isdir(trops_dir_expanded):
+    try:
+        os.makedirs(trops_dir_expanded)
+    except Exception as e:
+        raise SystemExit(f"ERROR: Unable to create {trops_dir}: {e}")
