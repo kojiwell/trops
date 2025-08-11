a0b132b64bcd3944e8adb08e27440958b7de2e78
Koji Tanaka <kojiwelly@gmail.com>
2025-08-11 15:14:58 +0900

    Update version to 0.2.28 in project files
    - Bumped version number in pyproject.toml, setup.py, and release.py to 0.2.28, reflecting the latest release.
    - This change ensures consistency across project files regarding the current version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="trops",
-    version="0.2.27",
+    version="0.2.28",
     author="Koji Tanaka",
     author_email="kojiwelly@gmail.com",
     description=("Track operations"),
