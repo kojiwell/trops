897428da0f8e0e329d3259795db2a572ba3efe5f
Koji Tanaka <kojiwelly@gmail.com>
2025-08-11 21:52:43 +0900

    chore(release): bump version to v0.2.29 and update CHANGELOG

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="trops",
-    version="0.2.28",
+    version="0.2.29",
     author="Koji Tanaka",
     author_email="kojiwelly@gmail.com",
     description=("Track operations"),
